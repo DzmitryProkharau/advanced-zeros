902ad00e9fc0f6415acf685b85828bd974e29554
DzmitryProkharau <33517308+DzmitryProkharau@users.noreply.github.com>
2018-03-02 23:26:50 +0300

    End example

--- src/index.js
+++ src/index.js
@@ -1,36 +1,22 @@
 module.exports = function getZerosCount(number, base) {
-	  	var zeros = 0;
-  	var arrNum = [];	
+	    var zeros = 0;
+    var remainder = 1;
+    var divisor = 2;
 
-  
-    for (var i = base; i > 0; i--) {
-    	for (var j = 1; j < base; j++) {
-    		if (j*i===base) {
-    			if (j%2!==0&&j>1) {
-    				if (parseInt(+(base+'').slice(-3)/13)===11) {
-    					arrNum.push(13);break;
-    				}else if (+(base+'').slice(-3)%7===0) {
-    					arrNum.push(7);break;
-    				}else {
-    					arrNum.push(j);console.log(j)
-    				}
-    			}else{
-    				if (j%2!==0) {arrNum.push(base);}    				
-    			}
-    		}
-    	}
+    while (base!==divisor) {
+        if (base%divisor === 0) {
+            remainder++;
+            base /= divisor;
+            
+        }else{
+            remainder = 1;
+            divisor++;            
+        }
     }
-
-    if (arrNum.length>1) {
-    	base = arrNum[arrNum.length-1]
-    }else {
-    	base = arrNum[0];
-    }
-
 
 	while( number > 0) {
-		number = number / base^0 ;
+		number = number / divisor^0 ;
 		zeros+=number;
 	}
-	return zeros;
+	return parseInt(zeros / remainder);
 }
